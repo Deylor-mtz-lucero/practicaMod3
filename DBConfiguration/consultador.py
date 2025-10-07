dfe0ee65c882456ba14968e22241fc565b22a380
Deylor-mtz-lucero <dmartinez.lucero@gmail.com>
2025-10-07 16:10:03 +0000

    consultador

--- DBConfiguration/consultador.py
+++ DBConfiguration/consultador.py
@@ -1,9 +1,28 @@
-import pyscopg2
+import psycopg2
 #Conexion a la base de datos
 
 conexion = psycopg2.connect(
     host="localhost",
-    database="nombre_base_datos",
-    user="usuario",
-    password="contraseña"
+    port="5432", # Puerto por defecto de PostgreSQL
+    database="RecursosHumanos",
+    user="Admin",
+    password="p4ssw0rdDB"
 )
+
+#cursor crear cursor
+cursor = conexion.cursor()
+
+#Ejecutar consulta
+cursor.execute("SELECT * FROM usuarios")
+
+#Obtener resultados
+registros = cursor.fetchall()   #fetchall recupera todas la filas devueltas
+                                # fetchone recupera una fila
+                                # fetchmany(n) recupera n filas
+#registros almacena en tuplas
+for fila in registros:
+    print(fila)
+    # Hacer algo con cada fila  
+#Cerrar cursor y conexion
+cursor.close()
+conexion.close()    
